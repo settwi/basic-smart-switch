a9f8422a14c87c22dfdd93ee018383e94a26be53
William Setterberg <william.s.setterberg@gmail.com>
2022-11-20 12:50:50 -0600

    cleanup on app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import queue
 import threading
 
-from common import schedule_fn
+import common
 import schedule_keeper as sk
 
 app = flask.Flask(__name__)
@@ -13,16 +13,17 @@ q = queue.Queue()
 
 
 def main():
+    common.init_logging(log_level=logging.DEBUG, log_fn='server-interface.log')
+
     t = threading.Thread(target=offline_logic)
     t.start()
+
     app.run(debug=False, host='0.0.0.0')
 
 
 def offline_logic():
     keeper = sk.ScheduleKeeper(
-        log_level=logging.DEBUG,
-        sched_fn=schedule_fn,
-        log_fn='server-interface.log',
+        sched_fn=common.schedule_fn,
         override_queue=q
     )
     keeper.loop()
@@ -36,8 +37,8 @@ def index():
 @app.route('/edit-schedule')
 def sched():
     dat = '{}'
-    if os.path.exists(schedule_fn):
-        with open(schedule_fn, 'r') as f:
+    if os.path.exists(common.schedule_fn):
+        with open(common.schedule_fn, 'r') as f:
             dat = json.loads(f.read())
     return flask.render_template('schedule.html', schedule=dat)
 
@@ -45,7 +46,7 @@ def sched():
 @app.route('/edit-schedule', methods=['POST'])
 def update_sched():
     new_schedule = flask.request.get_json()
-    with open(schedule_fn, 'w') as f:
+    with open(common.schedule_fn, 'w') as f:
         f.write(json.dumps(new_schedule))
     return flask.redirect(flask.url_for('sched'))
 
@@ -60,6 +61,15 @@ def override():
     except KeyError:
         logging.error('override dat corrupted')
     return flask.redirect(flask.url_for('sched'))
+
+
+@app.route('/override', methods=['GET'])
+def override_get():
+    '''
+    no reason for user to do a GET request but just in case
+    someone tries it give them a happy message
+    '''
+    return '<!DOCTYPE html> <html><h1>gtfo</h1></html>'
 
 
 if __name__ == '__main__':
