acd9d0fbc19a6090bde216f441b079a0427401e7
William Setterberg <william.s.setterberg@gmail.com>
2022-11-19 18:40:02 -0600

    add useful error output if the gpioset does not exist

--- toggle_switch_pin.sh
+++ toggle_switch_pin.sh
@@ -3,7 +3,14 @@
 # see https://docs.google.com/spreadsheets/d/1pAtz8v2gnkkrAdNrhhrpICkTNQ8cd1ObduUsphcbqp4/edit#gid=0
 # for a mapping between physical pin and
 # "Chip" and "Linux #"
-SWITCH_PIN=91
-CHIP=1
+switch_pin=91
+chip=1
 
-gpioset "$CHIP" "$SWITCH_PIN"="$1"
+
+if command -v gpioset
+then
+    gpioset "$chip" "$switch_pin"="$1"
+else
+    echo "gpioset does not exist; aborting" >&2
+    exit 1
+fi
