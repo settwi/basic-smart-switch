e85d51dc8a53a8b6cd9cc46b1775fc27f2021fb0
William Setterberg <william.s.setterberg@gmail.com>
2022-11-20 12:51:07 -0600

    move logging setup to common.py

--- common.py
+++ common.py
@@ -1,1 +1,14 @@
+import logging
+
 schedule_fn = 'schedule.txt'
+LOG_FMT = '%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s'
+DATE_FMT = '%Y-%m-%d %H:%M:%S'
+
+def init_logging(log_level: int, log_fn: str):
+    logging.basicConfig(
+        filename=log_fn,
+        level=log_level,
+        format=LOG_FMT,
+        datefmt=DATE_FMT,
+    )
+
